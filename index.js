52b83684179f03f557f936e8fb8d9ae1d8481412
Harshvardhan <hv9796923@gmail.com>
2024-09-25 15:29:42 +0530

    Updated object type data

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import express from "express"
 import 'dotenv/config'
 const app = express()
 // const port = 3000;
-
+const objData = {
+    "name": "John",
+    "address":"Basera Colony, near HP gas agency,Darbhanga, Bihar",
+    "email": "hv@123.com"
+}
 const port = process.env.PORT;
 app.get('/',(req,res) =>{
     res.send('Hello World')
 })
 app.get('/twitter',(req,res) =>{
     res.send('Twitter id is - noob_coder')
+})
+app.get('/user',(req,res) =>{
+    res.send(objData);
 })
 app.listen(port,(req,res) =>{
     console.log(`Server is running on port ${port}`)
